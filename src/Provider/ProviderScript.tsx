bd1dd7b481ecd9c5f41d373195a0ac191e890cb1
kafierence <vannam34760@gmail.com>
2024-04-16 21:55:47 +0700

    [update] provider

--- src/Provider/ProviderScript.tsx
+++ src/Provider/ProviderScript.tsx
@@ -1,8 +1,26 @@
+// Provider Script and Google
+
 import React from "react";
 
-// Provider Script Google Analysis
 const ProviderScript = () => {
-  return <div>ProviderScript</div>;
+  return (
+    <>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-4471GEE5EK"
+      ></script>
+
+      <script id="google-analytics">
+        {`     
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-4471GEE5EK');
+        gtag('send', 'pageview');
+        `}
+      </script>
+    </>
+  );
 };
 
 export default ProviderScript;
