6b1d544c58cb6a9777f1d338ab0c42ca4d1e8ca4
kafierence <vannam34760@gmail.com>
2024-04-28 16:21:19 +0700

    [update]

--- components/Button/PlayButton.tsx
+++ components/Button/PlayButton.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { StyledPlayButton } from "./Button.styles";
+import Image from "next/image";
 interface IProps {
   onClick: () => void;
 }
 const PlayButton = ({ onClick }: IProps) => {
-  return <StyledPlayButton onClick={onClick}>Play Game</StyledPlayButton>;
+  return (
+    <StyledPlayButton onClick={onClick}>
+      <Image src="/assets/icons/stark.svg" alt="" height={24} width={24} />
+      <span> {`Let's Stark`} </span>
+    </StyledPlayButton>
+  );
 };
 
 export default PlayButton;
