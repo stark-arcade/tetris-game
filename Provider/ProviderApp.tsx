70acb384f825b2b68d93dd501434acbb3d41225d
kafierence <vannam34760@gmail.com>
2024-05-29 14:05:32 +0700

    fix script

--- Provider/ProviderApp.tsx
+++ Provider/ProviderApp.tsx
@@ -5,12 +5,14 @@ import ProviderStarknet from "./ProviderStarknet";
 import ProviderWalletContext from "./ProviderWalletContext";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/themes";
+import ProviderScript from "./ProviderScript";
 
 const ProviderApp = ({ children }: PropsWithChildren) => {
   return (
     <ProviderStarknet>
       <ProviderWalletContext>
         <ChakraProvider theme={theme}>{children}</ChakraProvider>
+        <ProviderScript />
       </ProviderWalletContext>
     </ProviderStarknet>
   );
